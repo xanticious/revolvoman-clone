5b27dbe4c29276b34fab549815ef65af72786e50
xanticious <TonyLitchfield@Hotmail.com>
2025-09-05 23:55:15 -0600

    Added generateStaticParams to endless mode.

--- src/app/endless/[seed]/page.tsx
+++ src/app/endless/[seed]/page.tsx
@@ -1,5 +1,9 @@
 import EndlessLevelClient from './EndlessLevelClient';
 
+export async function generateStaticParams() {
+  return [];
+}
+
 export default function EndlessLevelPage() {
   return <EndlessLevelClient />;
 }
